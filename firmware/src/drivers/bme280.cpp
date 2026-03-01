e7f79a78fe3de4a2ba844c905f37ce814a3387ed
Jozef Jabczun <jabczunjozef@gmail.com>
2026-03-01 22:38:22 +0200

    fw: add BME280 example driver setup

---
+++ firmware/src/drivers/bme280.cpp
@@ -0,0 +1,15 @@
+#include <bme280.h>
+
+#include "drivers/bme280.h"
+
+// there is an example for using the bme driver here under examples:
+// https://github.com/boschsensortec/BME280_SensorAPI
+//
+// it might be worth to make this into a class
+
+void setup_bme280() {
+  // run setup code
+}
+void loop_bme280() {
+  // poll for data
+}
